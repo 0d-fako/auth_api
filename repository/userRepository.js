0b80aca3b74b7b3195b041a6baf2c1b3de7bde9a
0d-fako <od.fakorede@gmail.com>
2025-06-25 18:57:18 +0100

    auth - api

---
+++ repository/userRepository.js
@@ -0,0 +1,22 @@
+const User = require('../model/User');
+
+
+const findUserByEmail = async (email) => {
+  return await User.findOne({ email });
+};
+
+const createUser = async (userData) => {
+  const newUser = new User(userData);
+  return await newUser.save();
+};
+
+
+const findUserById = async (userId) => {
+  return await User.findById(userId).select('-password'); // exclude password
+};
+
+module.exports = {
+  findUserByEmail,
+  createUser,
+  findUserById
+};
